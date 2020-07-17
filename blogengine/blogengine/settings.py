8ec17321cec9d4c6b7b17ff94b12c73742240605
Serg <wpserggun@gmail.com>
2020-07-17 14:04:33 +0300

    Static files added manage.py collect static rewrite path to static files in linux

--- blogengine/blogengine/settings.py
+++ blogengine/blogengine/settings.py
@@ -117,5 +117,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blogengine\static'),
+    os.path.join(BASE_DIR, 'blogengine/static'),
 ]
